ee6e72b1ad116432fcffd4bbe936cf843126018f
sonchangwook <changw8k20@gmail.com>
2025-04-18 16:44:05 +0900

    start

---
+++ src/main/java/com/example/dashboard/AllInOneDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllInOneDashboardApplication.class, args);
+	}
+
+}
